5c5ad4f370458407bb2c3c8aa162c9200ad0ae35
JoseM <josemamaercado@gmail.com>
2025-10-24 11:40:19 +0200

    Cambio a L mayuscula

--- gestion_archivos.py
+++ gestion_archivos.py
@@ -1,7 +1,7 @@
 def de_lista_a_csv(l, nombre_fichero):
 
-    print("Funcion de_lista_a_csv prueba")
-    print("la lista es: ")
+    print("Funcion de_lista_a_csv:")
+    print("La lista es: ")
     print(l)
     print("En nombre del fichero es:")
     print(nombre_fichero)
