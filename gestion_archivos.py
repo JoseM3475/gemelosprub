396b4662dc90a38e9d1278dec5b7e4090553435f
JoseM <josemamaercado@gmail.com>
2025-10-24 11:26:13 +0200

    Comienzo trabajo Gestion Archivos

---
+++ gestion_archivos.py
@@ -0,0 +1,7 @@
+def de_lista_a_csv(l, nombre_fichero):
+
+    print("Funcion de_lista_a_csv")
+    print("la lista es: ")
+    print(l)
+    print("En nombre del fichero es:")
+    print(nombre_fichero)
